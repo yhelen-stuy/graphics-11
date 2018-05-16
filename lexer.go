6b3eb2a1ccb6291676c6e04f4f4bbd321777d20c
Helen Ye <00yhelen@gmail.com>
2018-05-15 23:46:27 -0400

    Add lexing fxns

--- lexer.go
+++ lexer.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strings"
+	"unicode"
 	"unicode/utf8"
 )
 
@@ -26,6 +28,7 @@ const (
 	T_STRING
 	T_NEWLINE
 	T_IDENTIFIER
+	T_EOF
 
 	LIHT
 	CONSTANTS
@@ -80,7 +83,7 @@ var eof = rune(0)
 type stateFn func(*Lexer) stateFn
 
 func (l *Lexer) run() {
-	for state := lexText; state != nil; {
+	for state := lexBase; state != nil; {
 		state = state(l)
 	}
 	close(l.tokens)
@@ -95,6 +98,62 @@ func Lex(input string) (*Lexer, chan Token) {
 	return l, l.tokens
 }
 
+// are these all the cases????
+func lexBase(l *Lexer) stateFn {
+	switch r := l.next(); {
+	case r == eof:
+		l.emit(T_EOF)
+		return nil
+	case r == '\n':
+		l.emit(T_NEWLINE)
+	case r == '/':
+		if l.peek() == '/' {
+			return lexComment
+		}
+		return lexString
+	case unicode.IsSpace(r):
+		l.ignore()
+	case r == '-' || 0 <= r && r <= 9:
+		l.backup()
+		return lexNumber
+		// Is this what I want?? hb letter/number only idrk
+	case unicode.IsPrint(r):
+		return lexString
+	}
+	// probably not the behavior i want lol
+	return nil
+}
+
+func lexNumber(l *Lexer) stateFn {
+	var ttype TokenType
+	l.accept("-")
+	digits := "0123456789"
+	l.acceptRun(digits)
+	if l.accept(".") {
+		l.acceptRun(digits)
+		ttype = T_FLOAT
+	} else {
+		ttype = T_INT
+	}
+	if unicode.IsLetter(l.peek()) {
+		return l.errorf("Invalid number")
+	}
+	l.emit(ttype)
+	return lexBase
+}
+
+func lexString(l *Lexer) stateFn {
+	// need to check:
+	// legal chars, if not has to be a string, else might be identifier
+	// continue reading until whitespace
+	return lexBase
+}
+
+func lexComment(l *Lexer) stateFn {
+	// read until newline
+	return lexBase
+}
+
 func (l *Lexer) next() rune {
 	if l.pos >= len(l.input) {
 		l.width = 0
@@ -104,6 +163,19 @@ func (l *Lexer) next() rune {
 	l.width = width
 	l.pos += width
 	return r
+}
+
+func (l *Lexer) accept(valid string) bool {
+	if strings.IndexRune(valid, l.next()) >= 0 {
+		return true
+	}
+	l.backup()
+	return false
+}
+
+func (l *Lexer) acceptRun(valid string) {
+	for l.accept(valid) {
+	}
 }
 
 func (l *Lexer) backup() {
@@ -118,6 +190,15 @@ func (l *Lexer) peek() rune {
 	r := l.next()
 	l.backup()
 	return r
+}
+
+func (l *Lexer) errorf(err string) stateFn {
+	// More robust error message lol
+	l.tokens <- Token{
+		ttype: T_ERROR,
+		val:   fmt.Sprintf("Syntax error: %s", err),
+	}
+	return nil
 }
 
 func (l *Lexer) emit(ttype TokenType) {
--- main.go
+++ main.go
@@ -5,11 +5,11 @@ import (
 )
 
 func main() {
-	image := MakeImage(500, 500)
-	t := MakeMatrix(4, 4)
-	t.Ident()
-	e := MakeMatrix(4, 0)
-	p := MakeMatrix(4, 0)
+	// image := MakeImage(500, 500)
+	// t := MakeMatrix(4, 4)
+	// t.Ident()
+	// e := MakeMatrix(4, 0)
+	// p := MakeMatrix(4, 0)
 	// ParseFile("galleryscript", t, p, e, image)
 	// ParseFile("script", t, p, e, image)
 }
